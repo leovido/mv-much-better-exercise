99711157480b79fcbd38d28f6bc51dd467c3cf38
Christian Ray Leovido <18484997+kuriishu27@users.noreply.github.com>
2024-08-16 18:19:35 +0100

    feat: finance tx feature

--- mv-much-better/mv_much_betterApp.swift
+++ mv-much-better/mv_much_betterApp.swift
@@ -1,32 +1,14 @@
-//
-//  mv_much_betterApp.swift
-//  mv-much-better
-//
-//  Created by Christian Leovido on 16/08/2024.
-//
-
 import SwiftUI
 import SwiftData
+import FinanceFeature
 
 @main
 struct mv_much_betterApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-        .modelContainer(sharedModelContainer)
-    }
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+				.environmentObject(FinanceDomain())
+				.preferredColorScheme(.light)
+		}
+	}
 }
